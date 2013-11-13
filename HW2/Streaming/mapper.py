f2cc8933a6d3f7a6dd1dff1f27e5e379097d98b4
Rex Cheung <rccheung@ucdavis.edu>
2013-11-12 18:31:37 -0800

    homework 2

---
+++ HW2/Streaming/mapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# From: http://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-python/
+
+import sys
+
+#f = open('C:\Users\user\School Work\Fall 2013\STA 250\HW2\Problem2\mini_out.txt')
+
+#This function truncates a number f to n decimals
+def trunc(f, n):
+    return str(f)[:len('%.*f' % (n, f))]
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    #sys.stdin
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    line = line.strip() # Need to remove trailing '\n'
+    entries = line.split('\t')
+    out_str = str(trunc(float(entries[0]),1)) + str(',') + str(float(trunc(float(entries[0]),1))+0.1) + str(',') + str(trunc(float(entries[1]),1)) + str(',') + str(float(trunc(float(entries[1]),1))+0.1) + str('\t') + str(1)
+
+    print out_str
+
---
+++ HW2/Streaming/reducer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from operator import itemgetter
+import sys
+
+current_key = None
+current_count = 1
+#f = open('C:\Users\user\School Work\Fall 2013\STA 250\HW2\Problem2\sorted.txt')
+#Set first line as initials
+#line = sys.stdin.readline()
+#line = line.strip()
+#key, count = line.split('\t')
+#keys = key.split(',')
+#xlo = float(keys[0]); xhi = float(keys[1]); ylo = float(keys[2]); yhi = float(keys[3]); count = int(count)
+#current_xlo = xlo; current_ylo = ylo;
+
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # parse the input we got from mapper.py
+    key, count = line.split('\t'); count = int(count)
+    #keys = key.split(',')
+    #xlo = float(keys[0]); xhi = float(keys[1]); ylo = float(keys[2]); yhi = float(keys[3]); count = int(count)
+    # convert count (currently a string) to int
+    try:
+        count = int(count)
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        continue
+
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+    if current_key == key:
+        current_count += count
+    else:       #new xlo
+        if current_key:
+            keys = current_key.split(',')
+            xlo = float(keys[0]); xhi = float(keys[1]); ylo = float(keys[2]); yhi = float(keys[3]); count = int(count)
+            print '%.1f,%.1f,%.1f,%.1f,%d' % (xlo,xhi,ylo,yhi,current_count)
+        current_count = count
+        current_key = key  #reset count
+
+# do not forget to output the last word if needed!
+if current_key == key:
+    print '%.1f,%.1f,%.1f,%.1f,%d' %(xlo,xhi,ylo,yhi,current_count)
+
+
